c9b9f73f17a1114b0e985fbc1463febdd22e7bfe
PrasadNayak04 <prasad.nayak@robosoftin.com>
2022-11-09 17:37:45 +0530

    marl

--- intern-management/src/main/java/com/robosoft/internmanagement/service/CandidateService.java
+++ intern-management/src/main/java/com/robosoft/internmanagement/service/CandidateService.java
@@ -9,6 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 public class CandidateService {
 
     public String candidateRegister(CandidateProfile candidateProfile, HttpServletRequest request){
-
+        return "";
     }
 }
