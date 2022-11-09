42fe06b20fd077885924fea2a4e9b5852488c8ce
Udith C Devadiga <udith.devadiga@robosoftin.com>
2022-11-09 13:15:09 +0530

    Test commit

--- intern-management/src/main/java/com/robosoft/internmanagement/controller/EmailController.java
+++ intern-management/src/main/java/com/robosoft/internmanagement/controller/EmailController.java
@@ -21,12 +21,8 @@ import java.nio.file.Paths;
 @RestController
 @RequestMapping("/intern-management")
 public class EmailController {
-
     @Autowired
     private EmailService emailService;
-
-    @Autowired
-    private StorageService storageService;
 
     @PostMapping("/send-otp")
     public ResponseEntity<?> sendMail(@RequestParam String toEmail){
@@ -38,42 +34,4 @@ public class EmailController {
            return ResponseEntity.status(HttpStatus.valueOf("Please provide valid email.")).build();
         }
     }
-
-    @PostMapping("/upload")
-    public String singleFileUpload(@RequestParam MultipartFile file, @RequestParam String email, HttpServletRequest request) {
-        return storageService.singleFileUpload(file, email, request);
-    }
-
-
-    @GetMapping("/fetch/{fileName}")
-    public ResponseEntity<Resource> getFile(@PathVariable String fileName, HttpServletRequest request) throws IOException {
-        final String filePath = "src\\main\\resources\\static\\documents\\" + fileName;
-        Path path = Paths.get(filePath);
-        Resource resource;
-        try {
-            resource = new UrlResource(path.toUri());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-            return null;
-        }
-
-        // Try to determine file's content type
-        String contentType = storageService.getContentType(request, resource);
-        /*try {
-            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
-            System.out.println(contentType);
-        } catch (IOException ex) {
-            System.out.println("Could not determine file type.");
-        }
-
-        // Fallback to the default content type if type could not be determined
-        if (contentType == null) {
-            contentType = "application/octet-stream";
-        }*/
-
-        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
-                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
-                .body(resource);
-    }
-
 }
