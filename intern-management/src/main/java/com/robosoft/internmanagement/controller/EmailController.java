7a3cbc55e97489e85e8b414fc7be8b343b40008d
PrasadNayak04 <prasad.nayak@robosoftin.com>
2022-11-09 11:11:47 +0530

    Candidate-Register first commit

--- intern-management/src/main/java/com/robosoft/internmanagement/controller/EmailController.java
+++ intern-management/src/main/java/com/robosoft/internmanagement/controller/EmailController.java
@@ -1,13 +1,22 @@
 package com.robosoft.internmanagement.controller;
 
 import com.robosoft.internmanagement.service.EmailService;
+import com.robosoft.internmanagement.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/intern-management")
@@ -15,6 +24,9 @@ public class EmailController {
 
     @Autowired
     private EmailService emailService;
+
+    @Autowired
+    private StorageService storageService;
 
     @PostMapping("/send-otp")
     public ResponseEntity<?> sendMail(@RequestParam String toEmail){
@@ -25,6 +37,43 @@ public class EmailController {
         }else{
            return ResponseEntity.status(HttpStatus.valueOf("Please provide valid email.")).build();
         }
+    }
+
+    @PostMapping("/upload")
+    public String singleFileUpload(@RequestParam MultipartFile file, @RequestParam String email, HttpServletRequest request) {
+        return storageService.singleFileUpload(file, email, request);
+    }
+
+
+    @GetMapping("/fetch/{fileName}")
+    public ResponseEntity<Resource> getFile(@PathVariable String fileName, HttpServletRequest request) throws IOException {
+        final String filePath = "src\\main\\resources\\static\\documents\\" + fileName;
+        Path path = Paths.get(filePath);
+        Resource resource;
+        try {
+            resource = new UrlResource(path.toUri());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        // Try to determine file's content type
+        String contentType = storageService.getContentType(request, resource);
+        /*try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+            System.out.println(contentType);
+        } catch (IOException ex) {
+            System.out.println("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }*/
+
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 
 }
