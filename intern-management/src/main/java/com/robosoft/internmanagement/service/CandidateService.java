c725d7bf9142724aa93d08ab3aa72c01a07c904d
PrasadNayak04 <111737831+PrasadNayak04@users.noreply.github.com>
2022-11-09 12:56:45 +0530

    candidate register git commit

---
+++ intern-management/src/main/java/com/robosoft/internmanagement/service/CandidateService.java
@@ -0,0 +1,14 @@
+package com.robosoft.internmanagement.service;
+
+import com.robosoft.internmanagement.modelAttribute.CandidateProfile;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+public class CandidateService {
+
+    public String candidateRegister(CandidateProfile candidateProfile, HttpServletRequest request){
+
+    }
+}
